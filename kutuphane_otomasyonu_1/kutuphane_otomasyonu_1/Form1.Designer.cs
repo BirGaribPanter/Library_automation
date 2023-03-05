71d57f93bdad1dd3262e6cbb6bb9d3fb25f82bcd
BirGaribPanter <116919080+BirGaribPanter@users.noreply.github.com>
2023-03-05 19:26:08 +0300

    Add files via upload

---
+++ kutuphane_otomasyonu_1/kutuphane_otomasyonu_1/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace kutuphane_otomasyonu_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "KİTAP ADI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "YAZAR ADI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(182, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "TARİH(İKİ BÖLÜMDE DE GEÇERLİ)";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(200, 155);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(590, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(636, 448);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 63);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(448, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 63);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "DURUM GÜNCELLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(336, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "ALAN KİŞİNİN ADI";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(464, 112);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(390, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "SERİ NO";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(464, 49);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(397, 75);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 17;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "VERİLDİ";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(476, 75);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 17);
+            this.radioButton2.TabIndex = 18;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "VERİLMEDİ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(689, 479);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(434, 20);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "DATAGRİDVİEW\'E TIKLANDIĞINDA VERİLERİ ÇEKİYOR";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(200, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 63);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "KİTAP SİL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(68, 8);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "SERİ NO";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(142, 1);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 21;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(247, 4);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(159, 13);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "-->SADECE SİLME İŞLEMİ İÇİN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 508);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ kutuphane_otomasyonu_1/kutuphane_otomasyonu_1/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace kutuphane_otomasyonu_1
+{
+    public partial class Form1 : Form
+    {
+        int seri = 0;
+        string durum = "0";
+        string tarih;
+        IDataReader dr;
+        OleDbConnection veri;
+        OleDbDataAdapter data;
+        OleDbCommand cmd;
+        DataSet ds;
+        void vericek()//verileri çekme işlemi
+        {
+
+            veri = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database9.accdb");
+            data = new OleDbDataAdapter("Select serino,kitap_adi,verildi_verilmedi,alan_adi,tarih,yazar from kutuphane", veri);
+            ds = new DataSet();
+            veri.Open();
+            data.Fill(ds, "kutuphane");
+            dataGridView1.DataSource = ds.Tables["kutuphane"];
+            veri.Close();
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Aqua;
+
+            dataGridView1.Columns[0].HeaderText = "serino";
+            dataGridView1.Columns[1].HeaderText = "kitap_adi";
+            dataGridView1.Columns[2].HeaderText = "verilme durumu";
+            dataGridView1.Columns[3].HeaderText = "alan kişinin adı";
+            dataGridView1.Columns[4].HeaderText = "tarih";
+            dataGridView1.Columns[5].HeaderText = "yazar";
+
+        }
+        void kontrol()
+        {
+            veri = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database9.accdb");
+            cmd = new OleDbCommand();
+            veri.Open();
+            cmd.Connection = veri;
+            cmd.CommandText = "SELECT * FROM kutuphane WHERE serino='" + textBox5.Text + "'";
+            dr = cmd.ExecuteReader();
+        }// Kayıt Kontrolü
+
+        void temizle()
+        {
+            dateTimePicker1.Value = Convert.ToDateTime(tarih);
+            radioButton2.Checked = true;
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            veri = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database9.accdb");
+            cmd = new OleDbCommand();
+            veri.Open();
+            cmd.Connection = veri;
+            cmd.CommandText = "SELECT COUNT(*) FROM kutuphane";
+            seri = Convert.ToInt32(cmd.ExecuteScalar())+1;
+            veri.Close();
+            if (textBox1.Text == "")
+            {
+                temizle();
+                MessageBox.Show("KİTAP BİLGİSİ GİRİN!", "HATA");
+            }
+            else if (textBox2.Text == "")
+            {
+                temizle();
+                MessageBox.Show(" KİTAP BİLGİSİ GİRİN!", "HATA");
+            }           
+
+            else
+            {
+                cmd = new OleDbCommand();
+                cmd.Connection = veri;
+                veri.Open();
+                cmd.CommandText = "insert into kutuphane (serino,kitap_adi,tarih,yazar) values ('" + seri.ToString() + "','" + textBox1.Text + "','" + dateTimePicker1.Value + "','"  + textBox2.Text + "')";
+                cmd.ExecuteNonQuery();
+                veri.Close();
+                temizle();
+                vericek();
+                MessageBox.Show("KİTAP EKLEME İŞLEMİ BAŞARILI!", "Ekleme İŞlemi Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            }
+        }
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox5.Text == "")
+            {
+                MessageBox.Show("KİTAP BİLGİSİ Boş Bırakılamaz!!", "Boş Bırakma Hatası");
+            }
+           
+            else if (textBox4.Text == "")
+            {
+                MessageBox.Show("KİŞİ ADI Boş Bırakılamaz!!", "Boş Bırakma Hatası");
+            }
+          
+
+
+            else
+            {
+                kontrol();
+
+                if (dr.Read())
+                {
+
+                    cmd = new OleDbCommand();
+
+                    cmd.Connection = veri;
+                    cmd.CommandText = "update kutuphane set verildi_verilmedi='" + durum.ToString() + "',alan_adi='" + textBox4.Text +  "' where serino='" + textBox5.Text + "'";
+                    cmd.ExecuteNonQuery();
+                    veri.Close();
+                    vericek();
+                    temizle();
+                    MessageBox.Show("VERİ Güncelleme İşlemi Başarılı!", "Güncelleme İŞlemi Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("VERİ Bulunamadı", "VERİ Bulunamama Hatası");
+                }
+
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            tarih = dateTimePicker1.Value.ToString();
+            vericek();
+            radioButton2.Checked = true;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox5.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            durum = "1";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            durum = "0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Length <= 0)
+            {
+                MessageBox.Show("SERİNO Bırakılamaz!!", "Boş Bırakma Hatası");
+            }
+            else
+            {
+
+                kontrol();
+
+                if (dr.Read())
+                {
+
+                    cmd = new OleDbCommand();
+
+                    cmd.Connection = veri;
+                    cmd.CommandText = "delete from kutuphane where serino='" + textBox3.Text + "'";
+                    cmd.ExecuteNonQuery();
+                    veri.Close();
+                    vericek();
+                    textBox5.Clear();
+                    MessageBox.Show("Seçilen VERİ Siilindi", "Silme İşlemi Başarılı");
+
+                }
+                else
+                {
+                    MessageBox.Show("Girilen VERİ Bulunamadı", "VERİ Bulunamama Hatası");
+                }
+
+
+                temizle();
+
+            }
+        }
+    }
+}//Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database9.accdb
